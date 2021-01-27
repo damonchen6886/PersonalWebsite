c712f2d3f12d9d11438bf7cd192198927f12242c
chen <guchennn@gmail.com>
2021-01-26 23:38:48 -0500

    add resume, updates

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -8,12 +8,12 @@ class Resume extends Component {
 
     if(this.props.data){
       var education = this.props.data.education.map(function(education){
-        return <div key={education.school}>  <h3>{education.school}⠀⠀<img className="company-pic"  src={"images/"+education.image} alt="Tim Baker Profile Pic" /></h3>
+        return <div key={education.school}>  <h3>{education.school}⠀⠀<img className="company-pic"  src={"images/"+education.image} alt="" /></h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
         <h6>{education.description}</h6><p>{education.Coursework}</p> {<p><b>{education.awards} </b>{education.honor}</p>}</div>
       })
       var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}  ⠀⠀<img className="company-pic"  src={"images/"+work.image} alt="Tim Baker Profile Pic" />  ⠀⠀ </h3>
+        return <div key={work.company}><h3>{work.company}  ⠀⠀<img className="company-pic"  src={"images/"+work.image} alt="" />  ⠀⠀ </h3>
            
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
             {work.description.map(item=>(
