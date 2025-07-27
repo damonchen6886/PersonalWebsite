7b15a2da243d46de26ac28df8873c4bc3e875c2b
damonchen6886 <guchennn@gmail.com>
2025-07-27 15:12:13 -0400

    add

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -45,6 +45,7 @@ class Header extends Component {
          </div>
       </div>
 
+
       <p className="scrolldown">
          <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
       </p>
